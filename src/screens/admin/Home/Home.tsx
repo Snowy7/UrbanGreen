cc42c29e210b062e286d0f80d92ae498f6b6fc38
Snowy <97126807+Snowy7@users.noreply.github.com>
2025-04-30 22:56:10 +0300

    Cont...

--- src/screens/admin/Home/Home.tsx
+++ src/screens/admin/Home/Home.tsx
@@ -68,7 +68,7 @@ const Home = () => {
                 width={moderateScale(30)}
                 height={moderateScale(30)}
               />
-              <TextComp text="Manage Greenspaces" style={styles.cardTitle} />
+              <TextComp text="Manage Green spaces" style={styles.cardTitle} />
             </TouchableOpacity>
             <TouchableOpacity 
               style={styles.card}
